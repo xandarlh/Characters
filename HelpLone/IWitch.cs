1738145c927e45a0aba5381c9c798937d899ec32
Alexandar Lackovic Hansen <alex303a@zbc.dk>
2020-11-10 11:00:50 +0100

    s

---
+++ HelpLone/IWitch.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelpLone
+{
+    interface IWitch
+    {
+        void RaiseShield();
+        void ShieldGlare();
+        void Teleport(int x, int y);
+    }
+}
---
+++ HelpLone/IWizard.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelpLone
+{
+    interface IWizard
+    {
+        void Teleport(int x, int y);
+        void ThrowFrostNova();
+        void ThrowMagicMisile();
+    }
+}
